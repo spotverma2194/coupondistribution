a8ef022e9975ee10ab6036d1b829b8fe12b60104
Deepak Verma <110923209+spotverma2194@users.noreply.github.com>
2025-03-15 22:26:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://coupondistribution-chi.vercel.app", credentials: true }));
 
 // Coupons List
 let coupons = [
